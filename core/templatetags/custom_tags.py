ed217b46383401c5177b3e5d5bab4bff3709a17f
FENG Hao <hiroshifuu@outlook.com>
2023-05-24 22:51:15 +0800

    feat✨: settings_value

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -1,7 +1,7 @@
 from django import template
+from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.defaultfilters import conditional_escape
-from django.template.loader import render_to_string
+from django.utils.html import conditional_escape
 
 
 register = template.Library()
@@ -10,11 +10,17 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def current_site_domain(context):
     request = context.get('request')
-
     try:
         site_domain = get_current_site(request).domain
     except AttributeError:
-        # This happens if request is None
-        # and sites framework is not in INSTALLED_APPS
+        # This happens if request is None and sites framework is not in INSTALLED_APPS
         site_domain = request.get_host()
     return conditional_escape(site_domain)
+
+
+@register.simple_tag
+def settings_value(name):
+    val = getattr(settings, name, None)
+    if val is None:
+        val = ""
+    return val
