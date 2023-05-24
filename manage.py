cec778bee36bfdadc192e01484469bcaceb0af20
FENG Hao <hiroshifuu@outlook.com>
2023-05-24 22:15:05 +0800

    chore🔧: clean up

--- manage.py
+++ manage.py
@@ -12,11 +12,7 @@ if __name__ == '__main__':
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        raise ImportError("Couldn't import Django. Are you sure it's installed and " "available on your PYTHONPATH environment variable? Did you " "forget to activate a virtual environment?") from exc
 
     # This allows easy placement of apps within the interior core directory.
     # current_path = os.path.dirname(os.path.abspath(__file__))
