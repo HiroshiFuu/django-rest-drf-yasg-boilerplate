bbe70531c4c44fca8d00ce6dd4eeef67801c0f92
FENG Hao <hiroshifuu@outlook.com>
2023-06-19 21:39:40 +0800

    feat✨: current_site_name tag

--- core/templatetags/custom_tags.py
+++ core/templatetags/custom_tags.py
@@ -2,9 +2,21 @@ from django import template
 from django.conf import settings
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.html import conditional_escape
-
+from django.utils.translation import gettext
 
 register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def current_site_name(context):
+    request = context.get('request')
+
+    try:
+        site_name = get_current_site(request).name
+    except AttributeError:
+        # This happens if request is None and sites framework is not in INSTALLED_APPS
+        site_name = gettext('my site')
+    return conditional_escape(site_name)
 
 
 @register.simple_tag(takes_context=True)
